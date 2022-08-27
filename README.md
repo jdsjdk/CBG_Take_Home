e0fec3dc484ccb18e3627d3aa547bc0852dec3a2
JessDevOps <jess.d.summerhill@gmail.com>
2022-08-27 03:25:56 -0400

    Setting up and creating the correct structure for my program. Then, I begin to implement the correct functionality.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CBG_Take_Home_JDS
- This is a take home quize
+ This is a take home quiz, and I am tasked of getting the correct dataset.
+
+Please see the "instructuions.txt" for a the total problem set.
---
+++ clean_dups.py
@@ -0,0 +1,31 @@
+'''
+Author: Jess Summerhill
+Project: This is a take home quiz for a job interview with CBG.
+Date: 8-26-2022
+'''
+
+from typing import List
+import pandas as pd
+from pathlib import Path as fp
+
+class CleanDupsCVS:
+    
+    # generic constructor
+    def __init__(self) -> None:
+        pass
+    
+    # get the in path, return the files list
+    def get_in_path(fname) -> List[str]:
+        csv_list = []
+        p_in = fp("in/")
+        
+        pass
+
+    def main() -> None:
+        print("Testing testing!")
+
+    if __name__ == "__main__":
+        main()
+        
+        
+clean_dup = CleanDupsCVS()
