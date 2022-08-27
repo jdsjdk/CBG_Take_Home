55d1b650c29f52a80cbd619c7704b280cc41c82c
JessDevOps <jess.d.summerhill@gmail.com>
2022-08-27 06:44:36 -0400

    Reads all of the files in the dir, returns the list

--- clean_dups.py
+++ clean_dups.py
@@ -4,28 +4,37 @@ Project: This is a take home quiz for a job interview with CBG.
 Date: 8-26-2022
 '''
 
-from typing import List
+import sys
 import pandas as pd
+from typing import List
 from pathlib import Path as fp
 
 class CleanDupsCVS:
     
     # generic constructor
     def __init__(self) -> None:
-        pass
-    
-    # get the in path, return the files list
-    def get_in_path(fname) -> List[str]:
-        csv_list = []
-        p_in = fp("in/")
-        
-        pass
+        pass     
 
     def main() -> None:
-        print("Testing testing!")
+        
+         # get the in path, return the files list
+        def get_path_files() -> List[str]:
+            
+            csv_list = []
+            in_path = fp().cwd()  / "in/"
+            files = in_path.rglob("*.csv")
+            
+            for file in files: 
+                csv_list.append(file.name)
+            
+            return csv_list
+        
+                
+        get_path_files()
+        
+        sys.exit(0)        
 
     if __name__ == "__main__":
         main()
-        
-        
+                
 clean_dup = CleanDupsCVS()
