d1795f2772645b5212197dd326d636c1627d48d8
JessDevOps <jess.d.summerhill@gmail.com>
2022-08-27 07:32:37 -0400

    Cleaned up README and instructions.txt Made serious progress on the py file.  Wrap it up tomorrow.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # CBG_Take_Home_JDS
  This is a take home quiz, and I am tasked of getting the correct dataset.
 
-Please see the "instructuions.txt" for a the total problem set.
+Please see the "~/info/instructuions.txt" for a the total problem set.
--- clean_dups.py
+++ clean_dups.py
@@ -4,6 +4,7 @@ Project: This is a take home quiz for a job interview with CBG.
 Date: 8-26-2022
 '''
 
+from cgi import test
 import sys
 import pandas as pd
 from typing import List
@@ -25,12 +26,20 @@ class CleanDupsCVS:
             files = in_path.rglob("*.csv")
             
             for file in files: 
-                csv_list.append(file.name)
+                csv_list.append(in_path / file.name)
             
             return csv_list
         
+        #Take in all of the CSV data, and then clean it all up
+        def clean_up_data(data_list: List[str]) -> None:
+            df1 = pd.read_csv(data_list[2])
+            print(df1)  #Stopping Tests for now.  Finish up tomorrow.
+            pass
+        
                 
-        get_path_files()
+        path_csvs = get_path_files()
+        clean_up_data(path_csvs)
+        
         
         sys.exit(0)        
 
